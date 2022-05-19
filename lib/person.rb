e6fb8a10e8b2d80f5078f78938552778e07d1133
Jennifer <jenniferroques@gmail.com>
2022-05-19 08:24:32 -0500

    commit tree b9d633a7847a7404325d2c5584dbe51adb69e461

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,23 @@
-# your code goes here
+class Person 
+    attn_accessor :bank_account
+    attn_reader :name, :happiness, :hygiene
+
+
+def initialize(name)
+    @name = name
+    @bank_account = 25
+    @happiness = 8 
+    @hygiene = 8 
+end 
+
+def happiness=(num)
+    @happiness = if num > 10
+                    10 
+    elsif num < 0 
+        0 
+    else 
+        num 
+    end 
+
+end 
+
